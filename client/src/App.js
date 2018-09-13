d9e828a97cd437696f473d343207174d95afc992
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-09-13 11:42:24 -0400

    react-router added and created basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
@@ -7,11 +10,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      	<Navbar />
-       	<Landing />
-				<Footer />
-      </div>
+    	<Router>
+	      <div className="App">
+	      	<Navbar />
+	       		<Route exact path="/" component={ Landing } />
+	       		<div className="container">
+							<Route exact path="/register" component={ Register } />
+							<Route exact path="/login" component={ Login } />
+	       		</div>
+					<Footer />
+	      </div>
+	    </Router>
     );
   }
 }
